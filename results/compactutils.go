906bf679bda77fd2c6304192a4709d94b467b21d
Mike Lapping <mlapping@gmail.com>
2014-11-12 00:39:25 -0500

    better error messages surrounding delimiters

--- results/compactutils.go
+++ results/compactutils.go
@@ -4,13 +4,18 @@ package results
 import (
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"strings"
 )
 
 var DecodeData = strings.Split
 
 func (reply *SearchReply) DecodeCompactMultiColumn() ([]map[string]string, error) {
-	keys, delimiter := reply.DecodeColumns()
+	keys, delimiter, err := reply.DecodeColumns()
+	if err != nil {
+		return nil, fmt.Errorf("Decoding Server Response failed. Actual RETS reply msg was %v. Error: %v",
+			reply.Text, err)
+	}
 
 	// iterate through each of the data elements
 	propertyData := make([]map[string]string, len(reply.Data))
@@ -32,15 +37,20 @@ func (reply *SearchReply) DecodeCompactMultiColumn() ([]map[string]string, error
 	return propertyData, nil
 }
 
-func (reply *SearchReply) DecodeColumns() ([]string, string) {
-	delimiter := reply.GetDelimiter()
-	return DecodeData(reply.Columns, delimiter), delimiter
+func (reply *SearchReply) DecodeColumns() ([]string, string, error) {
+	delimiter, err := reply.GetDelimiter()
+	if err != nil {
+		return nil, "", err
+	}
+	return DecodeData(reply.Columns, delimiter), delimiter, nil
 }
 
-func (reply *SearchReply) GetDelimiter() string {
+func (reply *SearchReply) GetDelimiter() (string, error) {
 	byteSlice, err := hex.DecodeString(reply.Delimiter.Value)
 	if err != nil {
-		return ""
+		return "", err
+	} else if len(byteSlice) == 0 {
+		return "", errors.New("Delimiter that the RETS Server returned is not valid")
 	}
-	return string(byteSlice[0])
+	return string(byteSlice[0]), nil
 }
